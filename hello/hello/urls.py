2a8797e3d12f2bb32334bdd5542f19053ecd7f7a
Your Name <you@example.com>
2024-10-08 17:50:40 +0300

    ПЗ-2
    Берлизев, Апухтин научились пользоваться параметрами представлений

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path
 from django.urls import re_path
 from firstapp import views
@@ -6,4 +7,8 @@ urlpatterns = [
  path('', views.index),
  re_path(r'^about', views.about),
  re_path(r'^contact', views.contact),
+ path('products/', views.products), # маршрут по умолчанию
+ path('products/<int:productid>/', views.products),
+ path('users/', views.users), # маршрут по умолчанию
+ path('users/<int:id>/<str:name>/', views.users),
 ]
