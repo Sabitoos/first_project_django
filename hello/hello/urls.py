786d79cf49a25c04dc8824291b599a95f0be0cc8
Your Name <you@example.com>
2024-10-14 10:27:26 +0300

    ЛЗ-5
    Берлизев, Апухтин приобрели общие сведения о параметрах строки запроса пользователя, переадресации и
    отправки пользователю статусных кодов

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
  path('', views.index),
  re_path(r'^about', views.about),
  re_path(r'^contact', views.contact),
- path('products/', views.products), # маршрут по умолчанию
  path('products/<int:productid>/', views.products),
- path('users/', views.users), # маршрут по умолчанию
- path('users/<int:id>/<str:name>/', views.users),
+ path('users/', views.users),
 ]
