0d16ec9ba6ebf79376b5d1c990aa924f29e3d6ea
sabito <levberlizev@gmail.com>
2024-10-30 15:01:39 +0300

    ПЗ 4
    Берлизев, Апухтин научились определять формы

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import *
+from .forms import UserForm
 
 def index(request):
- cat = ["Ноутбуки", "Принтеры", "Сканеры", "Диски", "Шнуры"]
- return render(request, "firstapp/index.html", context={"cat": cat})
+ if request.method == "POST":
+  name = request.POST.get("name") # получить значения поля Имя
+  age = request.POST.get("age") # значения поля Возраст
+  output = "<h2>Пользователь</h2><h3>Имя - {0}, Возраст – {1}</h3>".format(name, age)
+  return HttpResponse(output)
+ else:
+  userform = UserForm()
+  return render(request, "firstapp/index.html", {"form": userform})
+
 
 def about(request):
  return HttpResponse("<h2>О сайте</h2>")
