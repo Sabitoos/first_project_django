97d77ce4fc0c337ce81923cd659e1c06af274842
Your Name <you@example.com>
2024-11-07 14:18:24 +0300

    ЛЗ-9
    Берлизев, Апухтин приобрели общие понятия о работе с полями форм

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,26 +1,11 @@
-from django.shortcuts import render
 from django.http import *
 from .forms import UserForm
-
+from django.shortcuts import render
 def index(request):
  userform = UserForm()
+ if request.method == "POST":
+  userform = UserForm(request.POST)
+  if userform.is_valid():
+   name = userform.cleaned_data["name"]
+   return HttpResponse("<h2>Имя введено коррректно – {0}</h2>".format(name))
  return render(request, "firstapp/index.html", {"form": userform})
-
-
-
-def about(request):
- return HttpResponse("<h2>О сайте</h2>")
-
-def contact(request):
- return HttpResponse("<h2>Контакты</h2>")
-
-def products(request, productid):
- category = request.GET.get("cat", "")
- output = "<h2>Продукт № {0} Категория: {1}</h2>" .format(productid, category)
- return HttpResponse(output)
-
-def users(request):
- id = request.GET.get("id", 1)
- name = request.GET.get("name", "Максим")
- output = "<h2>Пользователь</h2><h3>id: {0} Имя: {1}</h3 >" .format(id, name)
- return HttpResponse(output)
