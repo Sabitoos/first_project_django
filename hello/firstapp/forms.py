d21046ff7de5fae020fc56389a6ef0857273a6b4
Your Name <you@example.com>
2024-11-07 13:48:14 +0300

    ПЗ-5
    Берлизев, Апухтин научились пользоваться специальными полями в формах

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -1,5 +1,8 @@
 from django import forms
-
 class UserForm(forms.Form):
- name = forms.CharField(label="Имя клиента")
- age = forms.IntegerField(label="Возраст клиента")
+ city = forms.TypedMultipleChoiceField(label="Выберите город",
+ empty_value=None,
+ choices=((1, "Москва"),
+ (2, "Воронеж"),
+(3, "Курск"),
+(4, "Томск")))
