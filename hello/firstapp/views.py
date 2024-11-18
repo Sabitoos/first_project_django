1617b973978265c65e5f57ef94643156e37459f1
sabito <levberlizev@gmail.com>
2024-11-18 04:37:52 +0300

    ПЗ-7
    Берлизев, Апухтин работали с объектами модели данных

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,11 +1,40 @@
-from django.http import *
-from .forms import UserForm
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import Person
+from django.http import HttpResponseNotFound
+
+
+# получение данных из БД и загрузка index.html
 def index(request):
- userform = UserForm()
+ people = Person.objects.all()
+ return render(request, "index.html", {"people": people})
+# сохранение данных в БД
+def create(request):
  if request.method == "POST":
-  userform = UserForm(request.POST)
-  if userform.is_valid():
-   name = userform.cleaned_data["name"]
-   return HttpResponse("<h2>Имя введено коррректно – {0}</h2>".format(name))
- return render(request, "firstapp/index.html", {"form": userform})
+  klient = Person()
+  klient.name = request.POST.get("name")
+  klient.age = request.POST.get("age")
+  klient.save()
+ return HttpResponseRedirect("/")
+# изменение данных в БД
+
+def edit(request, id):
+ try:
+  person = Person.objects.get(id=id)
+  if request.method == "POST":
+   person.name = request.POST.get("name")
+   person.age = request.POST.get("age")
+   person.save()
+   return HttpResponseRedirect("/")
+  else:
+   return render(request, "edit.html", {"person": person})
+ except Person.DoesNotExist:
+  return HttpResponseNotFound("<h2>Клиент не найден</h2>")
+# удаление данных из БД
+def delete(request, id):
+ try:
+  person = Person.objects.get(id=id)
+  person.delete()
+  return HttpResponseRedirect("/")
+ except Person.DoesNotExist:
+  return HttpResponseNotFound("<h2>Клиент не найден</h2>")
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -1,12 +1,9 @@
-from django.contrib import admin
 from django.urls import path
-from django.urls import re_path
 from firstapp import views
-from django.views.generic import TemplateView
 
 urlpatterns = [
  path('', views.index),
- path('about/', TemplateView.as_view(template_name="firstapp/about.html")),
- path('contact/', TemplateView.as_view(template_name="firstapp/contact.html",
- extra_context={"work": "Разработка программных продуктов"})),
+ path('create/', views.create),
+ path('edit/<int:id>/', views.edit),
+ path('delete/<int:id>/', views.delete),
 ]
