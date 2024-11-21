0df3a8a9d76f32c80d33bbf4a68189671a9c6bac
sabitoos <levberlizev@gmail.com>
2024-11-21 04:46:39 +0300

    ПЗ 9 0.5
    какая то ошибка

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from catalog import views
+from django.urls import re_path
+
 urlpatterns = [
- path('', views.index, name='home'),
- path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    re_path(r'^books/', views.BookListView.as_view(), name='books'),
+    re_path(r'^book-detail/', views.BookDetailView.as_view(), name='book-detail'),
 ]
