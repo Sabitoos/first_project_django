d3e3409686ff5e133d6bfd9d436abf132725a86f
Sabitoos <levberlizev@gmail.com>
2025-01-08 00:31:37 +0300

    ЛЗ-13

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from catalog import views
 from django.urls import re_path
+from django.urls import path, include
 
 urlpatterns = [
  path('', views.index, name='index'),
@@ -9,4 +10,5 @@ urlpatterns = [
  re_path(r'^books/$', views.BookListView.as_view(), name='books'),
  re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
  re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+ path('accounts/', include('django.contrib.auth.urls')),
 ]
